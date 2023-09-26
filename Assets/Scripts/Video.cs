5bd533864b84b0ad1f202dd55531ffe389efd571
Oleh20 <oleh10836@gmail.com>
2023-09-26 18:36:43 +0200

    initial commit

---
+++ Assets/Scripts/Video.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour
+{
+    public bool isSubtitles;
+
+    public bool isManyButtons;
+
+    public VideoClip videoOnButton;
+
+    public VideoClip videoOnSecondButton;
+
+    public Video nextVideoClass;
+    public Video secondNextVideoClass;
+}
